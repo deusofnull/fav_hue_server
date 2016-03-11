43a4bb8aea90bedf23d5ba157868dc6b49922dcd
Will Acheson <willyach07@gmail.com>
2016-03-10 19:42:02 -0500

    first commit, on tut 3

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+import datetime
+from django.utils import timezone
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
+
+# when updating models, these commands are needed:
+'''
+    python manage.py makemigrations polls # get create migrations to represent the changes made
+    python manage.py sqlmigrate polls 0001 # to check the sql statements (does nothing)
+    python manage.py migrate  # run the migrations
+'''
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    # ForeignKey defines a relationship between models
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
